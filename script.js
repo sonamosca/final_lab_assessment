57c561f250673a91c08ae18a9e16fab115fe1e7d
sonamosca <sonampelden050@gmail.com>
2024-11-27 10:57:49 +0530

    lab_assessment

---
+++ script.js
@@ -0,0 +1,23 @@
+deleteButton = document.getElementById('deleteStudentButton');
+// add an event listener to delete the last row/student on double clicking the deleteButton
+
+
+
+button = document.getElementById('addStudentButton');
+// add an event listener to style the button on click
+
+
+function showNewStudentForm(){
+// display the form on clicking the button
+
+
+}
+
+function validateForm(){
+    // validate your form
+    // regex for name: /^[A-Za-z]+(?: [A-Za-z]+)*$/  allows names habving one or more words with one white spaces in between
+    // regex for email:  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    // regex for student id: /^[a-zA-Z0-9]{7}$/  accepts alphanumeric values of 7 characters
+}
+
+
